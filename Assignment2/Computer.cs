a28672aa3012885e4c032f9e6bb968768a252ac3
RivkieKlein <43257799+RivkieKlein@users.noreply.github.com>
2019-03-07 11:36:14 -0500

    added methods

---
+++ Assignment2/Computer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    class Computer
+    {
+        readonly string iD;
+        private static string lastID;
+
+        private bool? antenna;
+        private double? hardDriveCap;
+        private int rAM;
+        public int?[] licensesPerSoftware;
+
+
+
+        public Computer()
+        {
+            if (lastID == null)
+            {
+                iD = "0";
+                lastID = iD;
+            }
+            else
+            {
+                iD = (Int32.Parse(lastID) + 1).ToString();
+                lastID = iD;
+            }
+        }
+
+
+
+
+        
+        public string ID { get { return iD } }
+
+
+        public bool Antenna { get; set; }
+
+        public double? HardDriveCap
+        {
+            get
+            {
+                return hardDriveCap;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    hardDriveCap = value;
+                }
+
+        
+                
+            }
+        }
+
+        public  int RAM
+        {
+            get
+            {
+                int amount = rAM;
+                if (antenna==true)
+                {
+                    amount = amount - 100;
+                }
+                else
+                {
+                    amount = amount - 50;
+                }
+                for (int c =0; c<licensesPerSoftware.Length; c++)
+                {
+                    if (licensesPerSoftware[c].HasValue)
+                    {
+                        if (licensesPerSoftware[c] > 0)
+                        {
+                            amount = amount + 10;
+                        }
+                    }
+                }
+            }
+            set
+            {
+                if (value >= 1000)
+                {
+                    rAM = value;
+                }
+            }
+        }
+    }
+}
--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -8,8 +8,65 @@ namespace Assignment2
 {
     class Program
     {
-        static void Main(string[] args)
+        private int CloudStorage = 500;
+        private int Netw
+        public static void Main(string[] args)
         {
+            
+        }
+
+        public static bool GetIntFromUser(out int number, int min, int max, int defaultVal)
+        {
+            Console.WriteLine("Please enter a number between" + min+" and "+ max);
+            int tempNumber = Convert.ToInt32(Console.ReadLine());
+
+            if (tempNumber >= min && tempNumber <= max)
+            {
+                number = tempNumber;
+                return true;
+            }
+            else
+            {
+                number = defaultVal;
+                return false;
+            }
+
+        }
+
+        public static bool DoubleIntNotPastMax(ref int number, int max, bool setToMax )
+        {
+            if ((number * 2) <= max)
+            {
+                number = number * 2;
+                return true;
+               
+            }
+            else
+            {
+                if (setToMax)
+                {
+                    number = max;
+                }
+                return false;
+            }
+        }
+
+        public static bool HalveValueNotPastMin(ref int number, int min, bool setToMin)
+        {
+            if ((number /2) >= min)
+            {
+                number = number / 2;
+                return true;
+            }
+            else
+            {
+                if (setToMin)
+                {
+                    number = min;
+                }
+                return false;
+                
+            }
         }
     }
 }
